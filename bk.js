02689312cfec2fa3829475e64e1eefc65456189e
kmakoto22 <43477276+kmakoto22@users.noreply.github.com>
2020-01-14 23:52:29 -0500

    Update bk.js

--- bk.js
+++ bk.js
@@ -1,21 +1,22 @@
-(function() {
-    var script = document.createElement("SCRIPT");
-    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js';
-    script.type = 'text/javascript';
-    script.onload = function() {
-        $('body').html(" ");
-        $('head').html(" ");
 
-        var rp = 'https://reverseproxystmary.herokuapp.com/';
 
-        var video = $.get(rp + 'download/?URL=' + window.location.href.replace('invidio.us', 'youtube.com'), function (res, status) {
-                return res;
-        });
-        var subtitle = $.get(rp + 'subtitle/?URL=' + window.location.href.replace('invidio.us', 'youtube.com'), function (res, status) {
-                return res;
-        });
-alert(video);
-        $('<video crossorigin="anonymous" style="width: 100vw; height: 100vh;" preload="auto" controls><source crossorigin="anonymous" src="' + video + '" type="video/webm"><track crossorigin="anonymous" src="' + video + '" kind="captions" srclang="en" label="English"></video>').appendTo('body');
-    };
-    document.getElementsByTagName("head")[0].appendChild(script);
-})();
+ var myWindow = window.open("", "_blank");
+        myWindow.document.write(`<html><head><script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script></head>
+<body>
+<script>
+        var url = prompt('URL to YT Video');
+
+function Sget(t,e){var n=new XMLHttpRequest;n.onreadystatechange=function(){4==this.readyState&&200==this.status&&e(this.responseText)},n.open("GET",t,!0),n.send()}function Smain(){var t="https://reverseproxystmary.herokuapp.com/";Sget(t+"download?URL=" + url,function(e){Sget(t+"subtits?URL=" + url,function(t){
+
+setTimeout(function () {
+$('<video crossorigin="anonymous" style="width: 100vw; height: 100vh;" preload="auto" controls><source crossorigin="anonymous" src="' + e + '" type="video/webm"><track crossorigin="anonymous" src="' + t + '" kind="captions" srclang="en" label="English"></video>').appendTo('body');
+}, 1000);
+
+})})}
+
+Smain();
+
+</script>
+</body>
+</html>`);
+        myWindow.focus();
