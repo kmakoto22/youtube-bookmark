d8fb9d83f5eb01c7a538c93cd9da1cda0a440a30
kmakoto22 <43477276+kmakoto22@users.noreply.github.com>
2020-01-31 08:34:24 -0500

    Update bk.js

--- bk.js
+++ bk.js
@@ -4,7 +4,7 @@
         myWindow.document.write(`<html><head><script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script></head>
 <body>
 <script>
-        var url = prompt('URL to YouTube or Invidio.us Video:').replace('invidio.us', 'youtube.com');
+        var url = prompt('URL to YouTube or Invidio.us Video:').replace('invidio.us', 'youtube.com').split("?v=")[1];
 
         function Smain(){
                 window.location.href = 'http://reverseproxystmary.herokuapp.com/video/viewVideoStmaryUrl.html?url=' + url;
