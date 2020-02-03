a529ac96c96461fdfe10a2d585c9a56ccbd2ea64
kmakoto22 <43477276+kmakoto22@users.noreply.github.com>
2020-02-03 14:22:17 -0500

    Update bk.js

--- bk.js
+++ bk.js
@@ -6,9 +6,10 @@
 <script>
         var url = prompt('URL to YouTube or Invidio.us Video:').replace('invidio.us', 'youtube.com').split("?v=")[1];
 
-        function Smain(){
-                window.location.href = 'http://reverseproxystmary.herokuapp.com/video/viewVideoStmaryUrl.html?url=' + url;
-        }
+        function Sget(t,e){var n=new XMLHttpRequest;n.onreadystatechange=function(){4==this.readyState&&200==this.status&&e(this.responseText)},n.open("GET",t,!0),n.send()}function Smain(){var t="https://reverseproxystmary.herokuapp.com/";Sget(t+"download?URL=" + url,function(e){Sget(t+"subtits?URL=" + url,function(t){
+document.body.innerHTML = '<iframe src="' + 'http://reverseproxystmary.herokuapp.com/video/viewVideoStmary.html?id=' + e + '&sid=' + t + '" style="width: 100vw; height: 100vh;"></iframe>';
+})})}
+Smain();
         Smain();
 
 </script>
