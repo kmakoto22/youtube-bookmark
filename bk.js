d698dfaa763f058b89eb5b436c5ac80465b9d4ca
kmakoto22 <43477276+kmakoto22@users.noreply.github.com>
2020-02-04 11:37:35 -0500

    Update bk.js

--- bk.js
+++ bk.js
@@ -1,1 +1,1 @@
-window.location.href = 'http://reverseproxystmary.herokuapp.com/video/viewVideoStmaryUrl.html?url='+window.location.href.replace("invidio.us", "youtube.com").replace("watch?v=", "watchv")+';
+window.location.href = 'https://reverseproxystmary.herokuapp.com/video/viewVideoStmaryUrl.html?url='+window.location.href.replace("invidio.us", "youtube.com").replace("watch?v=", "watchv")+'';
